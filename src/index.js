79bf54c07f3d4bac4d1ee0d493cf01b01328d455
t-bello7 <bello4aus@gmail.com>
2022-06-23 12:21:36 +0100

    add utility styles and style for elements

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@
 import './assets/css/style.css';
 import { createGame, createScore, getScores } from './modules/data.js';
 
-const scoreForm = document.querySelector('#score-form');
+const scoreForm = document.querySelector('.score-form');
 const scoreContainer = document.querySelector('.score-container');
-const refershBtn = document.querySelector('#refresh-btn');
+const refershBtn = document.querySelector('.refresh-btn');
 
 let id = await createGame('game');
 id = id.result.slice(14, 34);
 
+id="SYqQLsrGtDti2TMXSq3e"
+
 const renderElements = (arr, container) => {
   container.innerHTML = '';
   arr.forEach((element) => {
-    container.innerHTML += `<li>${element.user}:  ${element.score}</li>`;
+    container.innerHTML += `<li>  ${element.score} ${element.user} </li>`;
   });
 };
 
@@ -25,8 +27,13 @@ const refreshScore = async (id) => {
 scoreForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const user = scoreForm.elements.name.value;
+
   const score = scoreForm.elements.score.value;
-  await createScore(id, user, score);
+
+  if (user && score) {
+    await createScore(id, user, score); 
+  }
+ 
   scoreForm.reset();
 });
 
